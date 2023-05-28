13f967aa46c7be13d95421f73f85885f80c790d5
krishbale <sushil.pokharel.7528@gmail.com>
2023-05-28 14:36:44 +0545

    basic folder structure

---
+++ app/about/[id]/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+type Props = {
+  id?:number;
+}
+import React from 'react'
+import { useRouter } from 'next/navigation'
+
+const Page= ({}) => {
+  
+
+  
+
+  return (
+    <div>Album details for id:  </div>
+  )
+}
+
+export default Page;
