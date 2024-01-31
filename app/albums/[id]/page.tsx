48ed37cf2e7efe0afcc844e87cd2714282883919
krishbale <sushil.pokharel.7528@gmail.com>
2024-01-31 19:47:54 +0545

    updated formatting

--- app/albums/[id]/page.tsx
+++ app/albums/[id]/page.tsx
@@ -1,73 +1,75 @@
-'use client'
-import Loader from '../../../components/Preloader'
-import TrackList from '../../../components/TrackList'
-import React from 'react'
-import Image from 'next/image'
-import { useGetAlbumQuery } from '../../../redux/services/rapidapiCore'
-const Page= ({params}:any) => {
-  const ids = params.id
-  const {data,isFetching,error} = useGetAlbumQuery(ids);
-  if(isFetching) return (
-    <>
-    <Loader />
-    </>
-  )
-  if(error) return <div>Error Occured</div>
-  const album = data?.albums[0]
+"use client";
+import Loader from "../../../components/Preloader";
+import TrackList from "../../../components/TrackList";
+import React from "react";
+import Image from "next/image";
+import { useGetAlbumQuery } from "../../../redux/services/rapidapiCore";
+const Page = ({ params }: any) => {
+  const ids = params.id;
+  const { data, isFetching, error } = useGetAlbumQuery(ids);
+  if (isFetching)
+    return (
+      <>
+        <Loader />
+      </>
+    );
+  if (error) return <div>Error Occured</div>;
+  const album = data?.albums[0];
 
-  const {album_type,artists,copyrights,images,label,name,popularity,release_date,total_tracks,tracks} = album
-
-
-
-  
+  const {
+    album_type,
+    artists,
+    copyrights,
+    images,
+    label,
+    name,
+    popularity,
+    release_date,
+    total_tracks,
+    tracks,
+  } = album;
 
   return (
- 
-  
-      <div className="bg-white py-16">
-        <div className="container mx-auto px-4">
-            <div className="max-w-4xl  mx-auto text-center">
-                <h2 className="text-3xl font-semibold text-dark">{name}</h2>
-            <Image src={images[0].url} alt="Album Cover" width={64} height={64} className="rounded-full mx-auto" />
-                <h1 className="text-4xl font-bold text-dark mt-8">{label} </h1>
-             
-                {
-                  artists?.map((item:any,index:any)=>(
-                    <p className="text-lg text-dark mt-4" key={index}>{item.name}</p>
-                  ))
-                } 
-                <div className="flex justify-center mt-8">
-                    {/* <button className="bg-dark text-blue-500 px-6 py-2 rounded-full font-semibold hover:bg-blue-600 hover:text-dark">Play</button> */}
-                </div>
-            </div>
+    <div className="bg-white py-16">
+      <div className="container mx-auto px-4">
+        <div className="max-w-4xl  mx-auto text-center">
+          <h2 className="text-3xl font-semibold text-dark">{name}</h2>
+          <Image
+            src={images[0].url}
+            alt="Album Cover"
+            width={64}
+            height={64}
+            className="rounded-full mx-auto"
+          />
+          <h1 className="text-4xl font-bold text-dark mt-8">{label} </h1>
+
+          {artists?.map((item: any, index: any) => (
+            <p className="text-lg text-dark mt-4" key={index}>
+              {item.name}
+            </p>
+          ))}
+          <div className="flex justify-center mt-8">
+            {/* <button className="bg-dark text-blue-500 px-6 py-2 rounded-full font-semibold hover:bg-blue-600 hover:text-dark">Play</button> */}
+          </div>
         </div>
-        <div className="container mx-auto mt-16">
-            <div className="max-w-4xl mx-auto">
-                <h2 className="text-2xl text-dark font-bold mb-4">Tracklist</h2>
-                <ul>
-                  {tracks.items?.map((item:any,index:any)=>(
-                    <TrackList item={item} key={index} />
-                  ))}  
-                     
-                   
-           
-                    
-           
-                   
-           
-           
-                </ul>
-            </div>
+      </div>
+      <div className="container mx-auto mt-16">
+        <div className="max-w-4xl mx-auto">
+          <h2 className="text-2xl text-dark font-bold mb-4">Tracklist</h2>
+          <ul>
+            {tracks.items?.map((item: any, index: any) => (
+              <TrackList item={item} key={index} />
+            ))}
+          </ul>
         </div>
-        </div>
-   
-   
-   
-  )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Page;
-{/* <body className="bg-gray-100">
+{
+  /* <body className="bg-gray-100">
 <header className="bg-white shadow">
   <div className="container mx-auto px-4 py-4">
     <h1 className="text-2xl font-semibold text-gray-800">Album Details</h1>
@@ -88,7 +90,8 @@ export default Page;
         <li>Track 1</li>
         <li>Track 2</li>
         <li>Track 3</li>
-        {/* <!-- Add more tracks as needed --> */}
+        {/* <!-- Add more tracks as needed --> */
+}
 //       </ul>
 //     </div>
 //   </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,32 @@
-"use client"
-import Card from '../components/Card';
-import { useGetLatestAlbumQuery } from '../redux/services/rapidapiCore'
+"use client";
+import Card from "../components/Card";
+import { useGetLatestAlbumQuery } from "../redux/services/rapidapiCore";
 
-
-import Loader from '@/components/Preloader';
-
+import Loader from "@/components/Preloader";
 
 export default async function Home() {
-  const {data,isFetching,error} = useGetLatestAlbumQuery("Bollywood Top 10");
+  const { data, isFetching, error } =
+    useGetLatestAlbumQuery("Bollywood Top 10");
   const Top10Latest = data?.albums.items;
   console.log(Top10Latest);
-  if(isFetching) return (
-    <>
-    <Loader />
-    </>
-  )
-  if(error) return <div>error</div>
+  if (isFetching)
+    return (
+      <>
+        <Loader />
+      </>
+    );
+  if (error) return <div>error</div>;
 
-
- 
-
-  
-
-
-  return(
+  return (
     <div className="bg-white py-16">
-    <div className="container  mx-auto py-8">
-  <h1 className="text-3xl text-dark  font-bold mb-4">Trending Albums</h1>
-  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-
-     {Top10Latest?.map((item:any,index:any)=>(
-  <Card item={item} key={index} />
-    ))} 
-    
-      
-   
+      <div className="container  mx-auto py-8">
+        <h1 className="text-3xl text-dark  font-bold mb-4">Trending Albums</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {Top10Latest?.map((item: any, index: any) => (
+            <Card item={item} key={index} />
+          ))}
+        </div>
       </div>
-      </div>
-
-  </div>
-  
-  )
-     }
+    </div>
+  );
+}
